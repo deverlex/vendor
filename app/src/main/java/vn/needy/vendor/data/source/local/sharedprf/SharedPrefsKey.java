1d993997ca8d6a08bc9edcf9631742aee8ece81c
Spider man <nguyendo94vn@gmail.com>
2017-10-27 17:01:04 +0700

    Add UI design for profiles (company & personal)

--- app/src/main/java/vn/needy/vendor/data/source/local/sharedprf/SharedPrefsKey.java
+++ app/src/main/java/vn/needy/vendor/data/source/local/sharedprf/SharedPrefsKey.java
@@ -6,7 +6,8 @@ package vn.needy.vendor.data.source.local.sharedprf;
 
 public final class SharedPrefsKey {
     public static final String TOKEN_KEY = "TOKEN_KEY";
-    public static final String KEY_CREDENTIAL = "KEY_CREDENTIAL";
+    public static final String KEY_CREDENTIAL = "CREDENTIAL_KEY";
+    public static final String COMPANY_NUMBER_KEY = "COMPANY_NUMBER_KEY";
     private SharedPrefsKey() {
         // No-op
     }
