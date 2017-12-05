7b9cfbfcd9c9b25367ebbd7a0b81d20c1693133b
Deverlex <nguyendo94vn@gmail.com>
2017-12-05 21:21:08 +0700

    add account ui design.

---
+++ app/src/main/java/vn/needy/vendor/screen/account/AccountContract.java
@@ -0,0 +1,19 @@
+package vn.needy.vendor.screen.account;
+
+import vn.needy.vendor.screen.BasePresenter;
+import vn.needy.vendor.screen.BaseViewModel;
+
+/**
+ * Created by lion on 05/12/2017.
+ */
+
+class AccountContract {
+
+    interface ViewModel extends BaseViewModel<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
---
+++ app/src/main/java/vn/needy/vendor/screen/account/AccountViewModel.java
@@ -0,0 +1,25 @@
+package vn.needy.vendor.screen.account;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by lion on 05/12/2017.
+ */
+
+public class AccountViewModel extends BaseObservable implements AccountContract.ViewModel {
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void setPresenter(AccountContract.Presenter presenter) {
+
+    }
+}
