c3c3f48601ff187af6542c35a2b420a421589693
Deverlex <nguyendo94vn@gmail.com>
2017-12-08 13:59:54 +0700

    add file design for mainpage show products.

---
+++ app/src/main/java/vn/needy/vendor/screen/mainPage/itemProduct/ItemProductPnViewModel.java
@@ -0,0 +1,15 @@
+package vn.needy.vendor.screen.mainPage.itemProduct;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by lion on 08/12/2017.
+ */
+
+public class ItemProductPnViewModel extends BaseObservable {
+
+    public ItemProductPnViewModel() {
+
+    }
+
+}
