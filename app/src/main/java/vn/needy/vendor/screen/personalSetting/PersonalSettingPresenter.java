a74bafe67be68eac07fdf4ae95188330f710a87a
minh dai <haminhdai1997@gmail.com>
2017-12-30 12:01:02 +0700

    create personal setting

--- app/src/main/java/vn/needy/vendor/screen/personalSetting/PersonalSettingPresenter.java
+++ app/src/main/java/vn/needy/vendor/screen/personalSetting/PersonalSettingPresenter.java
@@ -1,8 +1,27 @@
 package vn.needy.vendor.screen.personalSetting;
 
+import android.content.Context;
+
 /**
  * Created by lion on 25/12/2017.
  */
 
-public class PersonalSettingPresenter {
+public class PersonalSettingPresenter implements PersonalSettingContract.Presenter{
+    private Context mContext;
+    private PersonalSettingContract.ViewModel mViewModel;
+
+    public PersonalSettingPresenter(Context mContext) {
+        this.mContext = mContext;
+        this.mViewModel = mViewModel;
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
 }
--- app/src/main/java/vn/needy/vendor/screen/personalSetting/PersonalSettingViewModel.java
+++ app/src/main/java/vn/needy/vendor/screen/personalSetting/PersonalSettingViewModel.java
@@ -1,8 +1,136 @@
 package vn.needy.vendor.screen.personalSetting;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+
+import vn.needy.vendor.R;
+import vn.needy.vendor.utils.dialog.DialogManager;
+
 /**
  * Created by lion on 25/12/2017.
  */
 
-public class PersonalSettingViewModel {
+public class PersonalSettingViewModel extends BaseObservable implements PersonalSettingContract.ViewModel{
+    private PersonalSettingContract.Presenter mPresenter;
+    private Activity mContext;
+    private DialogManager mDialogManager;
+    private boolean checkBirthday;
+    private boolean checkEmail;
+    private boolean checkSex;
+
+    public PersonalSettingViewModel( Activity mContext) {
+        this.mContext = mContext;
+        mDialogManager = new DialogManager(mContext);
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void setPresenter(PersonalSettingContract.Presenter presenter) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        mContext.onBackPressed();
+    }
+
+
+    @Override
+    public void onCheckedEmail() {
+        checkEmail = ! checkEmail;
+        notifyPropertyChanged(BR.checkEmail);
+        if (!checkEmail){
+            //mDialogManager.dialogWithNoTitleTwoButton(R.string.personal_setting_email , );
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setMessage(R.string.personal_setting_email);
+            builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            builder.setNegativeButton(R.string.cancel_open_location, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+    }
+
+    @Override
+    public void onCheckedBirthday() {
+
+        checkBirthday = ! checkBirthday;
+        notifyPropertyChanged(BR.checkBirthday);
+
+        if (!checkBirthday){
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setMessage(R.string.personal_setting_birthday);
+            builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            builder.setNegativeButton(R.string.cancel_open_location, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+    @Override
+    public void onCheckedSex() {
+
+        checkSex = ! checkSex;
+        notifyPropertyChanged(BR.checkSex);
+
+        if (!checkSex){
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setMessage(R.string.personal_setting_sex);
+            builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            builder.setNegativeButton(R.string.cancel_open_location, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+    @Bindable
+    public boolean isCheckBirthday() {
+        return checkBirthday;
+    }
+    @Bindable
+    public boolean isCheckEmail() {
+        return checkEmail;
+    }
+    @Bindable
+    public boolean isCheckSex() {
+        return checkSex;
+    }
 }
