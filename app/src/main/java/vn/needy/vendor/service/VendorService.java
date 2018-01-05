5f6f11edf228739bf51b211de067f1e7ba9fe2ac
Pham Quang Truong <truongphamit@gmail.com>
2018-01-05 13:49:09 +0700

    Get notification when app in background

--- app/src/main/java/vn/needy/vendor/service/VendorService.java
+++ app/src/main/java/vn/needy/vendor/service/VendorService.java
@@ -1,25 +1,83 @@
 package vn.needy.vendor.service;
 
-import android.app.Service;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.text.Html;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import vn.needy.vendor.R;
 
 /**
  * Created by lion on 05/01/2018.
  */
 
-public class VendorService extends Service {
+public class VendorService extends FirebaseMessagingService {
 
-    @Nullable
+    private static final String GCM_NOTIFICATION_TITLE = "google.c.a.c_l";
+    private static final String GCM_NOTIFICATION_BODY = "gcm.notification.body";
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.e(getClass().getName(), "onMessageReceived");
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        //TODO do something useful
-        return Service.START_STICKY;
+    public void handleIntent(Intent intent) {
+        vn.needy.vendor.domain.Notification notification =new vn.needy.vendor.domain.Notification();
+        try {
+            JSONObject notificationInfoJson = new JSONObject(intent.getStringExtra(GCM_NOTIFICATION_BODY));
+            notification.setTitle(notificationInfoJson.getString("title"));
+            notification.setHtmlContent(notificationInfoJson.getString("html_content"));
+            notification.setReferenceGUI(notificationInfoJson.getString("refecence_gui"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        assert notificationManager != null;
+        notificationManager.notify(0 /* ID of notification */, createNotification(notification));
+    }
+
+    private Notification createNotification(vn.needy.vendor.domain.Notification notification) {
+        Intent notiIntent;
+        PendingIntent pendingIntent = null;
+        try {
+            notiIntent = new Intent(this, Class.forName(getPackageName() + "." + notification.getReferenceGUI()));
+            notiIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, notiIntent,
+                    PendingIntent.FLAG_ONE_SHOT);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)
+                .setContentTitle(notification.getTitle())
+                .setContentText(Html.fromHtml(notification.getHtmlContent()))
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+        bigTextStyle.setBigContentTitle(notification.getTitle());
+        bigTextStyle.bigText(Html.fromHtml(notification.getHtmlContent()));
+
+        notificationBuilder.setStyle(bigTextStyle);
+
+        return notificationBuilder.build();
     }
 }
