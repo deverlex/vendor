5e937e3c35e1214c5e0ece80ee3b0b712e9421cf
Deverlex <nguyendo94vn@gmail.com>
2018-01-11 08:34:08 +0700

    add files design for sale index screen.

---
+++ app/src/main/java/vn/needy/vendor/screen/saleIndex/SaleIndexActivity.java
@@ -0,0 +1,10 @@
+package vn.needy.vendor.screen.saleIndex;
+
+import vn.needy.vendor.screen.BaseActivity;
+
+/**
+ * Created by lion on 11/01/2018.
+ */
+
+public class SaleIndexActivity extends BaseActivity {
+}
---
+++ app/src/main/java/vn/needy/vendor/screen/saleIndex/SaleIndexContract.java
@@ -0,0 +1,8 @@
+package vn.needy.vendor.screen.saleIndex;
+
+/**
+ * Created by lion on 11/01/2018.
+ */
+
+public interface SaleIndexContract {
+}
